dd6b2f9b2b0c75bc2799af3f39926e99588de768
noah-melin <noah.melin@lp2i-poitiers.fr>
2025-10-23 18:17:24 +0200

    main changes

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, encoding='utf-8') as file:
+        content = file.read()
+    return content
+
+def main():
+    book_path = 'path/to/book.txt'
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+main()
